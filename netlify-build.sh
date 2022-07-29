9bcfbfab60e16f54442269a44c0802db22ae9157
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2022-07-29 19:51:03 +0200

    Migrate to Antora 3 (#18)

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-# force local packages
-npm install ./packages/asciidoc-loader
-
-antora --html-url-extension-style=indexify --pull site.yml --generator ./lib/generator.js
+antora --html-url-extension-style=indexify site.yml
